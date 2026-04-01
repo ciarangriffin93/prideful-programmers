01363f10dc96e42b284ef4307a7c2b3cdc6ea1b3
CIARAN GRIFFIN <152191241+ciarangriffin93@users.noreply.github.com>
2026-04-01 12:19:03 +0100

    Fix static files (use whitenoise instead of cloudinary)

--- pridefulproject/settings.py
+++ pridefulproject/settings.py
@@ -146,6 +146,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
